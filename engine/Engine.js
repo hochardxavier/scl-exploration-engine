78de147e3523d9a51011e93f70a7e82ad7dc5c7a
hochardxavier <hochard.xavier@gmail.com>
2026-06-30 11:08:10 +0200

    Update Engine.js

--- engine/Engine.js
+++ engine/Engine.js
@@ -3,8 +3,8 @@ export default class Engine {
     static VERSION = "0.1.0-alpha1";
 
     log(message) {
-        console.log("SCL Exploration Engine "${Engine.VERSION});
-        console.log(${message);
+        console.log("SCL Exploration Engine " + ${Engine.VERSION});
+        console.log(message);
     }
 
     constructor() {
@@ -20,6 +20,9 @@ export default class Engine {
 
         this.canvas = document.getElementById("gameCanvas");
         this.ctx = this.canvas.getContext("2d");
+
+        this.canvas.width = window.innerWidth;
+        this.canvas.height = window.innerHeight;
 
         this.start();
 
@@ -49,7 +52,7 @@ export default class Engine {
             0,
             0,
             this.canvas.width,
-            this.canvas.heigt
+            this.canvas.height
         );
         
     }
