2d9d34ff1c5bbc870aa6706df4574694f45a735b
hochardxavier <hochard.xavier@gmail.com>
2026-06-30 10:14:55 +0200

    Update Engine.js

--- engine/Engine.js
+++ engine/Engine.js
@@ -8,6 +8,27 @@ export default class Engine {
 
         console.log("SCL Exploration Engine v0.1.0");
 
+        this.start();
+
+    }
+    
+    start () {
+        this.loop();
     }
 
+    loop() {
+        this.update();
+
+        this.render();
+
+        requestAnimationFrame(() => this.loop());
+    }
+
+    update() {
+
+    }
+
+    render() {
+
+    }
 }
