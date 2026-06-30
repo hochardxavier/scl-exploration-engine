5f2ba08cadfd00ac7ce8cd1316c068e8bd05ff4c
hochardxavier <hochard.xavier@gmail.com>
2026-06-30 11:13:48 +0200

    Fix string interpolation in log method

--- engine/Engine.js
+++ engine/Engine.js
@@ -3,7 +3,7 @@ export default class Engine {
     static VERSION = "0.1.0-alpha1";
 
     log(message) {
-        console.log('SCL Exploration Engine ${Engine.VERSION}');
+        console.log(`SCL Exploration Engine ${Engine.VERSION}`);
         console.log(message);
     }
 
