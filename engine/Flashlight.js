8e33e34ae06b50626e3964350de320740a9b5a84
hochardxavier <hochard.xavier@gmail.com>
2026-07-01 14:48:41 +0200

    Implement Flashlight class with basic functionality

--- engine/Flashlight.js
+++ engine/Flashlight.js
@@ -1,1 +1,51 @@
+export default class Flashlight {
 
+    constructor(engine) {
+
+        this.engine = engine;
+
+        this.x = 0;
+        this.y = 0;
+
+    }
+
+    init() {
+
+        const canvas = this.engine.canvas;
+
+        canvas.addEventListener("mousemove", (event) => {
+
+            const rect = canvas.getBoundingClientRect();
+
+            this.x = event.clientX - rect.left;
+            this.y = event.clientY - rect.top;
+
+        });
+
+    }
+
+    update() {
+
+    }
+
+    render() {
+
+        const ctx = this.engine.ctx;
+
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, 40, 0, Math.PI * 2);
+
+        ctx.fillStyle = "yellow";
+        ctx.fill();
+
+    }
+
+    stop() {
+
+    }
+
+    destroy() {
+
+    }
+
+}
