fbafa943d4212cdbc4c59686469576aef8fb9492
hochardxavier <hochard.xavier@gmail.com>
2026-07-01 13:22:40 +0200

    Add Scene class with constructor for scene data

--- engine/Scene.js
+++ engine/Scene.js
@@ -1,1 +1,12 @@
+export default class Scene {
 
+    constructor(data) {
+
+        this.id = data.id;
+        this.name = data.name;
+        this.background = data.background;
+        this.objects = data.objects;
+
+    }
+
+}
