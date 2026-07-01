2dbda0c2cc755420d57153b2641035b2b8cf7135
hochardxavier <hochard.xavier@gmail.com>
2026-07-01 13:23:15 +0200

    Add SceneLoader class to load scenes from JSON

--- engine/SceneLoader.js
+++ engine/SceneLoader.js
@@ -1,1 +1,15 @@
+import Scene from "./Scene.js";
 
+export default class SceneLoader {
+
+    async load(path) {
+
+        const response = await fetch(path);
+
+        const data = await response.json();
+
+        return new Scene(data);
+
+    }
+
+}
