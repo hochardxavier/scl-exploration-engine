38795e293a0c89f1dc085152206a4b6fae14ac92
hochardxavier <hochard.xavier@gmail.com>
2026-07-02 11:08:14 +0200

    Update Flashlight.js

--- engine/Flashlight.js
+++ engine/Flashlight.js
@@ -7,6 +7,9 @@ export default class Flashlight {
         this.x = 0;
         this.y = 0;
 
+        this.radius = 120;
+        this.intensity = 0.90;
+
     }
 
     init() {
@@ -31,12 +34,36 @@ export default class Flashlight {
     render() {
 
         const ctx = this.engine.ctx;
+        const canvas = this.engine.canvas;
+
+        // Sauvegarde l'état du canvas
+        ctx.save();
+
+        // Dessine un voile noir
+        ctx.fillStyle = `rgba(0,0,0,${this.intensity})`;
+        ctx.fillRect(
+            0,
+            0,
+            canvas.width,
+            canvas.height
+        );
+
+        // Le prochain dessin enlèvera des pixels
+        ctx.globalCompositeOperation = "destination-out";
 
         ctx.beginPath();
-        ctx.arc(this.x, this.y, 40, 0, Math.PI * 2);
+        ctx.arc(
+            this.x,
+            this.y,
+            this.radius,
+            0,
+            Math.PI * 2
+        );
 
-        ctx.fillStyle = "yellow";
         ctx.fill();
+
+        // Retour au mode normal
+        ctx.restore();
 
     }
 
