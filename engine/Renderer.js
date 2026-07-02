63f28380647dd1b4a228cbb1a44decbf38e041f7
hochardxavier <hochard.xavier@gmail.com>
2026-07-02 16:25:38 +0200

    Implement Renderer class for rendering engine

--- engine/Renderer.js
+++ engine/Renderer.js
@@ -1,1 +1,28 @@
+export default class Renderer {
 
+    constructor(engine) {
+
+        this.engine = engine;
+
+    }
+
+    render() {
+
+        const ctx = this.engine.ctx;
+        const canvas = this.engine.canvas;
+
+        // Dessine le fond
+        ctx.drawImage(
+            this.engine.background,
+            0,
+            0,
+            canvas.width,
+            canvas.height
+        );
+
+        // Dessine la lampe
+        this.engine.flashlight.render();
+
+    }
+
+}
