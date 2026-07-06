b0c08944f7bc27af0cf5b6d0b0ec02cf4027ded2
hochardxavier <hochard.xavier@gmail.com>
2026-07-06 16:17:11 +0200

    Update Renderer.js

--- engine/Renderer.js
+++ engine/Renderer.js
@@ -65,7 +65,7 @@ export default class Renderer {
                 x,
                 y,
                 width,
-                heigt
+                height
             );
 
             /*console.log(
