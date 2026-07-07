2d72426cea7f9cdf269e99f2c5be4d045d6efa9b
hochardxavier <hochard.xavier@gmail.com>
2026-07-07 16:12:07 +0200

    Update InteractionManager.js

--- engine/InteractionManager.js
+++ engine/InteractionManager.js
@@ -16,7 +16,27 @@ export default class InteractionManager {
 
         for (const object of this.engine.scene.objects) {
 
-            console.log(object.id);
+            const canvas = this.engine.canvas;
+
+            const width = canvas.width * object.width / 100;
+            const height = canvas.height * object.height / 100;
+
+            const centerX = canvas.width * object.x / 100;
+            const centerY = canvas.height * object.y / 100;
+
+            const left = centerX - width / 2;
+            const right = centerX + width / 2;
+
+            const top = centerY - height / 2;
+            const bottom = centerY + height / 2;
+
+            console.log({
+                id: object.id,
+                left,
+                right,
+                top,
+                bottom
+            });
 
         }
 
