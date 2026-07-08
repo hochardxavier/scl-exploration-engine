918c7ebc57c93df6046767f16fc40949dcdaeb60
hochardxavier <hochard.xavier@gmail.com>
2026-07-08 09:10:21 +0200

    Refactor interaction detection logic in InteractionManager

--- engine/InteractionManager.js
+++ engine/InteractionManager.js
@@ -21,32 +21,43 @@ export default class InteractionManager {
 
         for (const object of this.engine.scene.objects) {
 
-            const canvas = this.engine.canvas;
+                const canvas = this.engine.canvas;
 
-            const width = canvas.width * object.width / 100;
-            const height = canvas.height * object.height / 100;
+                const width = canvas.width * object.width / 100;
+                const height = canvas.height * object.height / 100;
 
-            const centerX = canvas.width * object.x / 100;
-            const centerY = canvas.height * object.y / 100;
+                const centerX = canvas.width * object.x / 100;
+                const centerY = canvas.height * object.y / 100;
 
-            const left = centerX - width / 2;
-            const right = centerX + width / 2;
+                const left = centerX - width / 2;
+                const right = centerX + width / 2;
 
-            const top = centerY - height / 2;
-            const bottom = centerY + height / 2;
+                const top = centerY - height / 2;
+                const bottom = centerY + height / 2;
 
-            if (
-                input.pointerX >= left &&
-                input.pointerX <= right &&
-                input.pointerY >= top &&
-                input.pointerY <= bottom
+                if (
+                    input.pointerX >= left &&
+                    input.pointerX <= right &&
+                    input.pointerY >= top &&
+                    input.pointerY <= bottom
+                ) {
+
+                    this.hoveredObject = object;
+
+                }
+
+        } //fin de la boucle FOR
+
+        const input = this.engine.inputManager;
+
+        if (
+            input.pointerJustPressed &&
+            this.hoveredObject
             ) {
+                this.clickedObject = this.hoveredObject;
+        } //fin du conditionnel IF
 
-                this.hoveredObject = object;
-
-            }
-
-        }
+        console.log(this.clickedObject?.id);
 
     }
 
