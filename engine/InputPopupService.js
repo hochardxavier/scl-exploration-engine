b23a31d2d84f4bd78769b75b2b9a658fa6c6b9ce
hochardxavier <hochard.xavier@gmail.com>
2026-07-13 11:16:54 +0200

    Add InputPopupService class for input handling

---
+++ engine/InputPopupService.js
@@ -0,0 +1,21 @@
+export default class InputPopupService {
+
+    constructor(engine) {
+
+        this.engine = engine;
+
+    }
+
+    init() {
+
+    }
+
+    show(inputData, callback) {
+
+        console.log(inputData);
+
+        callback("4268");
+
+    }
+
+}
