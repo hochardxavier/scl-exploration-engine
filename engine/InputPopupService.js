0aab8463f6b5873b7bfad8274fb854b8eb76d744
hochardxavier <hochard.xavier@gmail.com>
2026-07-13 11:49:40 +0200

    Comment out debug statements in show method
    Comment out console.log and callback calls in show method.

--- engine/InputPopupService.js
+++ engine/InputPopupService.js
@@ -12,10 +12,23 @@ export default class InputPopupService {
 
     show(inputData, callback) {
 
-        console.log(inputData);
+        //console.log(inputData);
 
-        callback("4268");
+        //callback("4268");
 
+        const overlay = document.createElement("div");
+        overlay.className = "scl-popup-overlay";
+
+        const popup = document.createElement("div");
+        popup.className = "scl-popup";
+
+        const title = document.createElement("h2");
+
+        const text = document.createElement("p");
+
+        const input = document.createElement("input");
+
+        const button = document.createElement("button");
          
 
     }
