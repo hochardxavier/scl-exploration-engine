63dde24c7596402afb090389a406ac9d66c40bdb
hochardxavier <hochard.xavier@gmail.com>
2026-07-15 09:43:04 +0200

    Create SceneContext class for scene management

---
+++ engine/SceneContext.js
@@ -0,0 +1,17 @@
+export default class SceneContext {
+
+    constructor() {
+
+        this.hostId = null;
+
+        this.sessionId = null;
+
+        this.sceneId = null;
+
+        this.inventory = [];
+
+        this.flags = {};
+
+    }
+
+}
