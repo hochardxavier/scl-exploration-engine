76c223fe40f26d96e60e331e0ccae8b00b558d5b
hochardxavier <hochard.xavier@gmail.com>
2026-07-15 09:53:27 +0200

    Add sceneId, inventory, and flags to SceneContext

--- engine/SceneContext.js
+++ engine/SceneContext.js
@@ -3,13 +3,31 @@ export default class SceneContext {
     constructor() {
 
         this.hostId = null;
-
         this.sessionId = null;
+        this.sceneId = null;
+        this.inventory = [];
+        this.flags = {};
 
+    }
+
+    load(context) {
+
+        this.hostId = context.hostId ?? null;
+        this.sessionId = context.sessionId ?? null;
+        this.sceneId = context.sceneId ?? null;
+
+        this.inventory = context.inventory ?? [];
+        this.flags = context.flags ?? {};
+
+    }
+
+    clear() {
+
+        this.hostId = null;
+        this.sessionId = null;
         this.sceneId = null;
 
         this.inventory = [];
-
         this.flags = {};
 
     }
