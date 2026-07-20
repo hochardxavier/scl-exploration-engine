b3a67821ea51ab53e7d84b62c86ccfa1f00bd844
hochardxavier <hochard.xavier@gmail.com>
2026-07-20 10:11:03 +0200

    Add TODO for replacing test context in app.js
    Added a TODO comment to replace test context with communication protocol.

--- app.js
+++ app.js
@@ -7,5 +7,11 @@ await engine.init();
 
 const hostService = new HostService(engine);
 
+// TODO : Remplacer ce contexte de test par le protocole de communication.
+console.log ("TODO : Remplacer ce contexte de test par le protocole de communication -- apps.js ligne10"); 
+const context = {
+    sceneId: "scene02"
+};
+
 await hostService.receive(context);
 
